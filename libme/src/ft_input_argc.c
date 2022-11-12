8c4b2e7a3c39bc271138878b970776f0cb93d50a
Jakob Willert <jwillert@3-D-3.42heilbronn.de>
2022-11-12 14:11:16 +0100

    test

--- libme/src/ft_input_argc.c
+++ libme/src/ft_input_argc.c
@@ -1,35 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_input_len.c                                     :+:      :+:    :+:   */
+/*   ft_input_argc.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/24 15:48:24 by jwillert          #+#    #+#             */
-/*   Updated: 2022/11/12 13:37:20 by jwillert         ###   ########.fr       */
+/*   Created: 2022/11/12 13:42:03 by jwillert          #+#    #+#             */
+/*   Updated: 2022/11/12 13:45:39 by jwillert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libme.h"
 
-size_t	ft_input_len(char **array_to_count)
+size_t	ft_input_argc(char **array_to_count)
 {
-	size_t	index_array;
-	size_t	index_str;
-	size_t	input_length;
+	size_t	index;
 
-	index_array = 0;
-	index_str = 0;
-	input_length = 0;
-	while (array_to_count[index_array] != NULL)
-	{
-		while (array_to_count[index_array][index_str] != '\0')
-		{
-			index_str++;
-		}
-		input_length = input_length + index_str;
-		index_str = 0;
-		index_array++;
-	}
-	return (input_length);
+	index = 0;
+	while (array_to_count[index] != NULL)
+		index++;
+	return (index);
 }
--- libme/src/ft_input_strjoin.c
+++ libme/src/ft_input_strjoin.c
@@ -6,28 +6,47 @@
 /*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:48:45 by jwillert          #+#    #+#             */
-/*   Updated: 2022/10/24 15:48:49 by jwillert         ###   ########.fr       */
+/*   Updated: 2022/11/12 14:10:06 by jwillert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libme.h"
 
+static char	*ft_merge(char **argv, char *str_merge)
+{
+	size_t	index_array;
+	size_t	index_str;
+	size_t	input_length;
+
+	index_str = 0;
+	index_array = 0;
+	input_length = 0;
+	while (argv[index_array] != NULL)
+	{
+		while (argv[index_array][index_str] != '\0')
+		{
+			str_merge[input_length] = argv[index_array][index_str];
+			input_length++;
+			index_str++;
+		}
+		str_merge[input_length++] = ' ';
+		index_str = 0;
+		index_array++;
+	}
+	str_merge[input_length - 1] = '\0';
+	return (str_merge);
+}
+
 char	*ft_input_strjoin(char **argv)
 {
-	size_t	index;
 	char	*str_return;
 
 	if (argv == NULL)
 		return (NULL);
-	index = 0;
-	str_return = ft_str_empty();
+	str_return = (char *) malloc (sizeof (char) * ft_input_len(argv)
+			+ ft_input_argc(argv));
 	if (str_return == NULL)
 		return (NULL);
-	while (argv[index] != NULL)
-	{
-		str_return = ft_strjoin(str_return, argv[index]);
-		str_return = ft_strjoin(str_return, " ");
-		index++;
-	}
+	str_return = ft_merge(argv, str_return);
 	return (str_return);
 }
