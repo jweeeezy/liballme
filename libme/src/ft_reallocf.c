2dbd28bbec2f73761909175cff8cf583609b1ade
Jakob Willert <jwillert@3-D-3.42heilbronn.de>
2022-11-16 14:53:01 +0100

    added ft_reallocf

--- libme/src/ft_reallocf.c
+++ libme/src/ft_reallocf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*   ft_reallocf.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 15:52:23 by jwillert          #+#    #+#             */
-/*   Updated: 2022/11/16 13:24:45 by jwillert         ###   ########.fr       */
+/*   Updated: 2022/11/16 14:21:14 by jwillert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,21 @@ void	*ft_reallocf(void *ptr_to_realloc, size_t size_old, size_t size_new)
 {
 	void	*ptr_return;
 
-
-	// if not enough room to enlarge the room pointed to by ptr...
-		// new allocation with old data and freeing the old allocation
-
-
-
-
-
-
-	// reallocf will free the passed pointer if the requested memory cannot be allocated
-
-
-
-// if ptr == NULL its equal to malloc for size bytes
 	if (ptr_to_realloc == NULL)
 	{
 		ptr_return = ((void *) malloc (size_new));
 		free (ptr_to_realloc);
 		return (ptr_return);
 	}
-
-// if size is zero and ptr != NULL a new minimum sized object is allocated and the original object is freed
-	if (size_new == 0 && ptr != NULL)
+	if (size_new == 0 && ptr_to_realloc != NULL)
 	{
-		ptr_return = ((void *) malloc (1));
+		ptr_return = NULL;
 		free(ptr_to_realloc);
 		return (ptr_return);
 	}
-
-
-	ptr_temp = (void *)malloc(size_new);
-	if (ptr_temp == NULL)
-		return (NULL);
-	ft_memmove(ptr_temp, ptr_to_realloc, size_old);
+	ptr_return = (void *)malloc(size_new);
+	if (ptr_return != NULL)
+		ft_memmove(ptr_return, ptr_to_realloc, size_old);
 	free(ptr_to_realloc);
-	return (ptr_temp);
+	return (ptr_return);
 }
