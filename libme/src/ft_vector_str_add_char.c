db47d0d3d95edacca70845308509734a1d1dcc81
jweeeezy <willertjakob@gmail.com>
2022-12-15 22:33:32 +0100

    new func for reworked push_swap

--- libme/src/ft_vector_str_add_char.c
+++ libme/src/ft_vector_str_add_char.c
@@ -16,22 +16,29 @@ t_vector_str	*ft_vector_str_add_char(t_vector_str *vector_to_expand, char c_to_a
 {
 	t_vector_str	*vector_return;
 	size_t			size_to_add;
+	char			*str_temp;
+	char			*str_to_add;
 
 	if (vector_to_expand == NULL)
 
 	size_to_add = 2;
 	if (vector_to_expand->size_used + size_to_add <= vector_to_expand->size_allocated)
 	{
-		ft_strlcat(vector_to_expand->str, )
+		vector_to_expand ->str[vector_return->size_used + 1] = c_to_add;
+		vector_to_expand ->str[vector_return->size_used + 2] = '\0';
+		vector_to_expand->size_used = vector_to_expand->size_used + 2;
 	}
-
-
-
-
-
-
-
-
-
-
+	else
+	{
+		str_temp = malloc (sizeof (char) * 2);
+		if (str_temp == NULL)
+			return (str_temp);
+		str_temp[0] = c_to_add;
+		str_temp[1] = '\0';
+		str_to_add = ft_strjoin(vector_to_expand->str, str_temp);
+		vector_return = ft_vector_str_new(str_to_add);
+		ft_vector_str_free(vector_to_expand);
+		free(str_temp);
+	}
+	return (vector_return);
 }
